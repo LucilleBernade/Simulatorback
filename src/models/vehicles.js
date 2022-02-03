23adb5ccf02309e8093a087d079e99a4ff527e28
LucilleBernade <bernadelucille@gmail.com>
2022-02-03 21:06:23 +0100

    tentatives malheureuses

--- src/models/vehicles.js
+++ src/models/vehicles.js
@@ -1,7 +1,26 @@
 const connexion = require('../connexion');
 
-const findPrice = (type, id_regions) => connexion.promise().query('SELECT * FROM vehicles WHERE type=? AND id_regions=?', [type, id_regions]);
-// query pas séparée
+const findVehicle = (type, id_regions) => connexion.promise().query('SELECT * FROM vehicles WHERE type=? AND id_regions=?', [type, id_regions]);
+
+//const findAllTypeVehicles = (type) => connexion.promise().query('SELECT * FROM vehicles ');
+
+//const findVehicle = (type, id_regions) => {
+//  let query = 'SELECT * FROM vehicles';
+//  const params = [];
+//
+//  if (type) {
+//  query += ` WHERE type=${type}`;
+//  }
+//
+//  if (id_regions) {
+//    query += ` WHERE id_region=${id_regions}`;
+//    }
+//  
+//  return connexion.promise().query(query, params);
+//};
+
+
+
 module.exports = {
-  findPrice,
+  findVehicle,
 };
--- src/routers/vehicles.js
+++ src/routers/vehicles.js
@@ -2,7 +2,7 @@ const vehiclesRouter = require('express').Router();
 const vehiclesModel = require('../models/vehicles');
 
 vehiclesRouter.get('/', async (req, res) => {
-    const [price] = await vehiclesModel.findPrice(req.query.type, req.query.id_regions);
+    const [price] = await vehiclesModel.findVehicle(req.query.type, req.query.id_regions);
     res.json(price);
   });
 
